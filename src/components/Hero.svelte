f21ea1b1723ddbd0c604bc3b214b531cd54843a4
Ryan Holmes <ryan.holmes@stsaviationgroup.com>
2021-07-26 10:47:03 -0400

    Initial commit

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,11 +5,7 @@
 
 <div class="py-[180px] bg-hero bg-cover bg-center flex flex-col">
     <div class="flex flex-col justify-self-center items-center">
-        <img
-            class="rounded-full h-48 mb-8"
-            src="/images/avatar.png"
-            alt="Ryan's Face"
-        />
+        <img class="rounded-full h-48 mb-8" src="/images/avatar.png" alt="Ryan's Face" />
         <h2 class="text-2xl sm:text-4xl px-4 font-bold mb-2 text-white text-shadow text-center">
             {title}
         </h2>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,24 @@
-<h1 class="text-3xl text-[#cedb00]">Testing!</h1>
+<script context="module">
+    export const prerender = true;
+
+    export async function load({ page, fetch, session, context }) {
+        let res = await fetch('/posts/recent-posts.json');
+
+        return {
+            props: {
+                posts: await res.json()
+            }
+        };
+    }
+</script>
+
+<script>
+    import Hero from '../components/Hero.svelte';
+    import BlogSnippets from '../components/BlogSnippets.svelte';
+
+    export let posts;
+</script>
+
+<Hero title="Ryan Holmes" subTitle="Floridaman Developer" />
+
+<BlogSnippets {posts} title="Recent Posts" />
